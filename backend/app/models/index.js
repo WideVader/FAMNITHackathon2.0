ee5f8fc4b4df08ae9c40faadb973f36c339f8c28
WideVader <sever.1.trifunovic@gmail.com>
2023-05-28 01:38:05 +0200

    improved backend drastically

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
 const refreshToken = require('./refreshToken.model')
 const user = require('./user.model')
+const listing = require('./listing.model')
 // mongoose.Promise = global.Promise;
 
 const db = {};
 
 db.mongoose = mongoose;
 
-user = require("./user.model");
 db.role = require("./role.model");
-refreshToken = require("./refreshToken.model");
 
-// db.ROLES = ["user", "lord"];
+// db.ROLES = ["user", "lord"];kojem se educiraju studenti o financial literacy ovdje u kopru
 
 
 
 module.exports = {
-    refreshToken
+    user,
+    refreshToken,
+    listing
 };
+{/* <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC72f9xNJZm6ZB-5jfgT6Ld3tZc1zjMrpc&callback=initMap"     async defer></script>
+*/}
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const mongoose = require('mongoose');
 const User = require('./app/models/user.model');
 const Role = require('./app/models/role.model');
+const Listing = require('./app/models/listing.model');
 const routes = require('./app/routes');
 
 mongoose.set("strictQuery", false);
@@ -20,36 +22,24 @@ app.use(express.json());
 //   name: "Lord"
 // });
 
-// const user1 = new User({
-//   username: "Branko",
-//   email: "branko@example.com",
-//   password: "janko123",
-//   roles: []
+// const list1 = new Listing({
+//   adress: "Veluščkova ulica 7, 6000 Koper",
+//   price: 250,
+//   url: "placeholder",
+//   title: "Koper stan",
+//   amenities: [
+//       {wifi: true},
+//       {breakfast: false},
+//       {parking:true},
+//       {pets:true}
+//   ]
 // })
+
+// list1.save();
 
 // roleLord.save()
 // user1.save()
 
-
-/////////////////////
-// User Connection //
-/////////////////////
-
-// app.post('/register', async (req, res) => {
-//   const user = new User(req.body);
-//   try {
-//     await user.save();
-//     res.status(201).json(user);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// })
-
-// app.post('/login', (req, res) => {
-//   const user = new User(req.body);
-//   user.save();
-//   res.status(201).json({ user });
-// })
 
 ///////////
 // API's //
@@ -120,9 +110,117 @@ app.post('/api/users/new', async (req, res) => {
   }
 })
 
-app.get('/api/roles/', async (req, res) => {
-  const result = await Role.find();
-  res.send(result);
+app.get('/api/listings/', async (req, res) => {
+  // const result = await Role.find();
+  // res.send(result);
+
+  const responses = [];
+  try {
+    const result = await Listing.find({ adress: { $regex: `${req.body.adress}`, $options: 'i' } });
+
+    for (let i = 0; i < result.length; i++) {
+      const element = result[i];
+      const baseUrl = 'https://nominatim.openstreetmap.org/search';
+      const params = {
+        q: element.adress,
+        format: 'json',
+        limit: 1
+      };
+
+      await axios.get(baseUrl, { params })
+        .then(async (response) => {
+          if (response.data.length === 0) {
+            console.error('No results found for the address:', element.adress);
+            // return;
+          }
+          //IT TOOK 3 HOURS TO MAKE THESE AWAIT ASYNC WORK, NEVER AGAIN PLEASE!!!!
+          const { lat, lon } = response.data[0];
+          await responses.push(await universal(lat, lon, req.body.filter, element.adress))
+
+        })
+        .catch((error) => {
+          console.error('Error occurred while geocoding:', error);
+        });
+
+    }
+
+    res.send(responses)
+    return
+
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+
+  async function sendOverpassRequest(query, amenities, rad) {
+    try {
+      const response = await axios.get('https://overpass-api.de/api/interpreter', {
+        params: {
+          data: query,
+        },
+      });
+      if (rad != 0 && response.data.elements.length > 0) {
+        return response.data.elements[0].lat + ", " + response.data.elements[0].lon;
+      }
+      return null
+    } catch (error) {
+      console.error('Error occurred while sending Overpass request:', error);
+    }
+  }
+
+  async function universal(lat, lon, rad, el) {
+    try {
+      let amenities = ["'shop'='convenience'", "'amenity'='restaurant'", "'highway'='bus_stop'", "'tourism'='museum'", "'leisure'='park'", "'amenity'='school'", "'leisure'='stadium'", "'leisure'='fitness_centre'", "'amenity'='fuel'", "'aeroway'='aerodrome'"]
+      let response = []
+      let responseObj = { adress: el, filters: [] }
+      let flag = true;
+      for(let i = 0; i < amenities.length; i++){
+        if(rad[i]!=0){
+          const query = `[out:json];
+          node[${amenities[i]}](around:${rad[i]},${lat},${lon});
+          out;`;
+          const resultingSmt = await sendOverpassRequest(query, amenities[i], rad[i])
+          if (resultingSmt == null) {
+            flag = false
+            throw new Error('BreakException');
+          }
+        }
+      }
+      for (let i = 0; i < amenities.length; i++) {
+        if (rad[i] == 0) {
+          const query = `[out:json];
+          node[${amenities[i]}](around:2000,${lat},${lon});
+          out;`;
+          const resultingSmt = await sendOverpassRequest(query, amenities[i], 2000)
+          if (resultingSmt != null)
+            responseObj.filters.push(amenities[i].split("=").pop())
+          // response.push({ adress: el, amenities: amenities[i], latLonString: resultingSmt });
+        }
+        else {
+          const query = `[out:json];
+          node[${amenities[i]}](around:${rad[i]},${lat},${lon});
+          out;`;
+          const resultingSmt = await sendOverpassRequest(query, amenities[i], rad[i])
+          if (resultingSmt == null) {
+            flag = false
+            throw new Error('BreakException');
+          }
+          responseObj.filters.push(amenities[i].split("=").pop())
+
+          // response.push({ adress: el, amenities: amenities[i], latLonString: resultingSmt });
+        }
+
+      }
+      response.push(responseObj)
+      if (!flag) {
+        console.log("FLAG FALSE")
+        return []
+      }
+      console.log("FLAG TRUE")
+      return response
+    } catch (error) {
+      console.log("error")
+    }
+  }
 })
 
 ///////////////////////
@@ -141,3 +239,5 @@ const init = async () => {
 }
 
 init();
+
+
